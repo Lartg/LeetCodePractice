5ad19ab7e75517a0e2bc673dbd7f916f41fb780b
Lartg <roc.gerrety@gmail.com>
2022-03-28 20:58:32 -0700

    intuited new problem, solving tomorrow

---
+++ shift-2d-grid.py
@@ -0,0 +1,12 @@
+'''
+
+Given a grid and shift operation,
+return a new grid
+
+intuition: 
+  determine the size of the grid, assign numerical 
+  value to each location.
+  shift each element by the shift operation,
+  if the shift brings the element to a point beyond, loop to start
+
+'''
